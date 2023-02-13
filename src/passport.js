5d268de323c87ad6f069c1a90a33d76c690615c0
nalin243 <devpriyanalin@gmail.com>
2023-02-13 13:48:24 +0530

    Implemented authentication using jsonwebtokens

---
+++ src/passport.js
@@ -0,0 +1,21 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+
+const passport = require('passport')
+const User = require('../models/UserSchema')
+
+const options = {
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: process.env.SECRETKEY
+}
+
+passport.use(new JwtStrategy(options,(jwtPayload,done)=>{
+	console.log("entering auth")
+	User.findOne({username:jwtPayload.username})
+		.then((user)=>{
+			if(!user)
+				done(null,false)
+			else 
+				done(null,user)
+		})
+}))
--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 const PORT=process.env.PORT 
 const HOST=process.env.HOST 
 
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const express = require('express')
 const bcrypt = require('bcrypt')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const User = require('../models/UserSchema')
 mongoose.connect(process.env.DB_URL)
@@ -14,17 +16,28 @@ mongoose.connect(process.env.DB_URL)
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use(passport.initialize())
 
+require('./passport')
 
 app.post('/register',(req,res)=>{
-	bcrypt.hash(req.body.password,10,(err,hash)=>{
-		if(err)
-			console.log(err)
+	User.findOne({username:req.body.username})
+		.then((user)=>{
+			if(user){
+				res.status(200).send({
+					success:false,
+					message:"User already exists!"
+				})
+			}
+			else{
+				bcrypt.hash(req.body.password,10,(err,hash)=>{
+				if(err)
+					console.log(err)
 
-		const user = new User({
-			username: req.body.username,
-			password: hash 
-		})
+				const user = new User({
+					username: req.body.username,
+					password: hash 
+		})	
 		user.save()
 	})
 
@@ -32,8 +45,45 @@ app.post('/register',(req,res)=>{
 		success:true,
 		message:"New user registered"
 	})
+			}
+		})
 })
 
+app.post("/login",(req,res)=>{
+	User.findOne({username:req.body.username})
+		.then((user)=>{
+			if(!user){
+				res.status(400).send({
+					success:false,
+					message:"User not found"
+				})
+			}
+			else {
+				bcrypt.compare(req.body.password,user.password,(err,result)=>{
+					if(!result){
+						res.status(400).send({
+							success:false,
+							message:"Incorrect password!"
+						})
+					}
+					else {
+
+						const payload = {
+							username: user.username,
+							id: user._id
+						}
+
+						const token = jwt.sign(payload,process.env.SECRETKEY,{expiresIn:'1d'})
+						res.status(200).send({
+							sucess:true,
+							message:"Logged in!",
+							token: "Bearer "+token
+						})
+					}
+				})
+			}
+		})
+})
 
 app.listen(PORT,HOST,()=>{
 	console.log(`Server started on ${HOST} on port ${PORT}`)
