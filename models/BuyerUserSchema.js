fb05ad33a45f457cb7739b44da31106d08fa2e32
nalin243 <devpriyanalin@gmail.com>
2023-02-17 21:34:59 +0530

    Updated schemas

---
+++ models/BuyerUserSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const {Schema,model} = mongoose
+
+
+const ItemSchema = new Schema({
+	name:{type:String,required:true},
+	price:{type:Number,required:true},
+	description: String, 
+	sellername: String
+})
+
+const BuyerUserSchema = new Schema({
+	username: {type:String,required:true},
+	password: {type:String,required:true,minLength:[8,'Must be atleast of length 8']},
+	email: {type:String,required:true},
+	Cart:[ItemSchema]
+
+})
+
+module.exports = model('BuyerUser',BuyerUserSchema)
